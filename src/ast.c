60adeaaa36eb290d82364d357198ea837c3dc74c
samedit66 <samedit66@yandex.ru>
2024-12-11 00:39:23 +0300

    refactor(parser): декомпозировал

---
+++ src/ast.c
@@ -0,0 +1,330 @@
+#include "./include/json.h"
+
+void
+add_type_to_node(Json *node, char *type_name) {
+    Json_add_string_to_object(node, "type", type_name);
+}
+
+Json*
+mk_empty() {
+    Json *node = Json_new();
+    add_type_to_node(node, "empty");
+    return node;
+}
+
+Json*
+mk_int_const(int val) {
+    Json *node = Json_new();
+    add_type_to_node(node, "int_const");
+    Json_add_int_to_object(node, "value", val);
+    return node;
+}
+
+Json*
+mk_char_const(int val) {
+    Json *node = Json_new();
+    add_type_to_node(node, "char_const");
+    Json_add_int_to_object(node, "value", val);
+    return node;
+}
+
+Json*
+mk_real_const(double val) {
+    Json *node = Json_new();
+    add_type_to_node(node, "real_const");
+    Json_add_double_to_object(node, "value", val);
+    return node;
+}
+
+Json*
+mk_string_const(char *val) {
+    Json *node = Json_new();
+    add_type_to_node(node, "string_const");
+    Json_add_string_to_object(node, "value", val);
+    return node;
+}
+
+Json*
+mk_result_const() {
+    Json *node = Json_new();
+    add_type_to_node(node, "result_const");
+    return node;
+}
+
+Json*
+mk_current_const() {
+    Json *node = Json_new();
+    add_type_to_node(node, "current_const");
+    return node;
+}
+
+Json*
+mk_boolean_const(bool val) {
+    Json *node = Json_new();
+    add_type_to_node(node, "boolean_const");
+    Json_add_bool_to_object(node, "value", val);
+    return node;
+}
+
+Json*
+mk_void_const() {
+    Json *node = Json_new();
+    add_type_to_node(node, "void_const");
+    return node;
+}
+
+Json*
+mk_ident_lit(char *ident) {
+    Json *node = Json_new();
+    add_type_to_node(node, "ident_lit");
+    Json_add_string_to_object(node, "value", ident);
+    return node;
+}
+
+Json*
+mk_bin_op(char *op_name, Json *left, Json *right) {
+    Json *node = Json_new();
+    add_type_to_node(node, op_name);
+    Json_add_object_to_object(node, "left", left);
+    Json_add_object_to_object(node, "right", right);
+    return node;
+}
+
+Json*
+mk_unary_op(char *op_name, Json *arg) {
+    Json *node = Json_new();
+    add_type_to_node(node, op_name);
+    Json_add_object_to_object(node, "arg", arg);\
+    return node;
+}
+
+Json*
+mk_simple_call(char *feature_name, Json *args_list) {
+    Json *simple_call = Json_new();
+    Json_add_string_to_object(simple_call, "name", feature_name);
+    Json_add_array_to_object(simple_call, "args_list", args_list);
+    return simple_call;
+}
+
+Json*
+mk_list() {
+    return Json_new();
+}
+
+Json*
+add_to_list(Json *list, Json *element) {
+    Json_add_object_to_array(list, element);
+    return list;
+}
+
+Json*
+mk_simple_call_no_args(char *feature_name) {
+    return mk_simple_call(feature_name, mk_list());
+}
+
+Json*
+mk_precursor_args_call(Json *args_list) {
+    Json *node = Json_new();
+    add_type_to_node(node, "precursor_call");
+    Json_add_array_to_object(node, "args_list", args_list);
+    return node;
+}
+
+Json*
+mk_precursor_no_args_call() {
+    return mk_precursor_args_call(mk_list());
+}
+
+Json*
+mk_feature_with_owner_call(Json *owner, Json *feature) {
+    Json *node = Json_new();
+    add_type_to_node(node, "feature_call");
+    
+    if (owner != NULL)
+        Json_add_object_to_object(node, "owner", owner);
+    else
+        Json_add_object_to_object(node, "owner", mk_empty());
+    Json_add_object_to_object(node, "feature", feature);
+    return node;
+}
+
+Json*
+mk_feature_with_unknown_owner_call(Json *feature) {
+    return mk_feature_with_owner_call(NULL, feature);
+}
+
+Json*
+mk_bracket_access(Json *source, Json *index) {
+    Json *node = Json_new();
+    add_type_to_node(node, "bracket_access");
+    Json_add_object_to_object(node, "source", source);
+    Json_add_object_to_object(node, "index", index);
+    return node;
+}
+
+Json*
+mk_if_expr(Json *cond, Json *then_expr, Json *alt_exprs, Json *else_expr) {
+    Json *node = Json_new();
+    add_type_to_node(node, "if_expr");
+    Json_add_object_to_object(node, "cond", cond);
+    Json_add_object_to_object(node, "then_expr", then_expr);
+    Json_add_array_to_object(node, "elseif_exprs", alt_exprs);
+    Json_add_object_to_object(node, "else_expr", else_expr);
+    return node;
+}
+
+Json*
+add_elseif_expr(Json *alts, Json *cond, Json *expr) {
+    Json *alt = Json_new();
+    add_type_to_node(alt, "elseif_expr");
+    Json_add_object_to_object(alt, "cond", cond);
+    Json_add_object_to_object(alt, "expr", expr);
+    Json_add_object_to_array(alts, alt);
+    return alts;
+}
+
+Json*
+mk_program(Json *program) {
+    Json *node = Json_new();
+    add_type_to_node(node, "root");
+    Json_add_array_to_object(node, "program", program);
+    return node;
+}
+
+Json*
+mk_if_stmt(Json *cond, Json *then_stmt_list, Json *alt_stmts, Json *else_stmt_list) {
+    Json *node = Json_new();
+    add_type_to_node(node, "if_stmt");
+    Json_add_object_to_object(node, "cond", cond);
+    Json_add_object_to_object(node, "then_clause", then_stmt_list);
+    Json_add_array_to_object(node, "elseif_clauses", alt_stmts);
+    Json_add_object_to_object(node, "else_clause", else_stmt_list);
+    return node;
+}
+
+Json*
+add_elseif_stmt(Json *alts, Json *cond, Json *stmt_list) {
+    Json *alt = Json_new();
+    add_type_to_node(alt, "elseif_clause");
+    Json_add_object_to_object(alt, "cond", cond);
+    Json_add_object_to_object(alt, "body", stmt_list);
+    Json_add_object_to_array(alts, alt);
+    return alts;
+}
+
+Json*
+mk_assign_stmt(Json *left, Json *right) {
+    Json *node = Json_new();
+    add_type_to_node(node, "assign_stmt");
+    Json_add_object_to_object(node, "left", left);
+    Json_add_object_to_object(node, "right", right);
+    return node;
+}
+
+Json*
+mk_loop_stmt(Json *init_stmt_list, Json *cond, Json *body_stmt_list) {
+    Json *node = Json_new();
+    add_type_to_node(node, "loop_stmt");
+    Json_add_object_to_object(node, "init", init_stmt_list);
+    Json_add_object_to_object(node, "cond", cond);
+    Json_add_object_to_object(node, "body", body_stmt_list);
+    return node;
+}
+
+Json*
+mk_inspect_stmt(Json *expr, Json *when_clauses, Json *else_stmt_list) {
+    Json *node = Json_new();
+    add_type_to_node(node, "inspect_stmt");
+    Json_add_object_to_object(node, "expr", expr);
+    Json_add_array_to_object(node, "when_clauses", when_clauses);
+    Json_add_object_to_object(node, "else_clause", else_stmt_list);
+    return node;
+}
+
+Json*
+add_alt_when_clause(Json *when_clauses, Json *choices, Json *body) {
+    Json *when_stmt = Json_new();
+    Json_add_array_to_object(when_stmt, "choices", choices);
+    Json_add_object_to_object(when_stmt, "body", body);
+    Json_add_object_to_array(when_clauses, when_stmt);
+    return when_clauses;
+}
+
+Json*
+mk_choice_interval(Json *start, Json *end) {
+    Json *node = Json_new();
+    add_type_to_node(node, "choice_interval");
+    Json_add_object_to_object(node, "start", start);
+    Json_add_object_to_object(node, "end", end);
+    return node;
+}
+
+Json*
+mk_type(char *type_name) {
+    Json *user_type = Json_new();
+    add_type_to_node(user_type, "type_spec");
+    Json_add_string_to_object(user_type, "type_name", type_name);
+    return user_type;
+}
+
+Json*
+mk_integer_type() {
+    return mk_type("INTEGER");
+}
+
+Json*
+mk_real_type() {
+    return mk_type("REAL");
+}
+
+Json*
+mk_string_type() {
+    return mk_type("STRING");
+}
+
+Json*
+mk_character_type() {
+    return mk_type("CHARACTER");
+}
+
+Json*
+mk_boolean_type() {
+    return mk_type("BOOLEAN");
+}
+
+Json*
+mk_like_type(Json *like_what) {
+    Json *type_spec_like = Json_new();
+    add_type_to_node(type_spec_like, "type_spec_like");
+    Json_add_object_to_object(type_spec_like, "like_what", like_what);
+    return type_spec_like;
+}
+
+Json*
+mk_like_current_type() {
+    return mk_like_type(mk_current_const());
+}
+
+Json*
+mk_like_other_field_type(char *field_name) {
+    return mk_like_type(mk_ident_lit(field_name));
+}
+
+Json*
+mk_generic_user_type(char *type_name, Json *type_list) {
+    Json *generic_type_spec = Json_new();
+    add_type_to_node(generic_type_spec, "generic_type_spec");
+    Json_add_string_to_object(generic_type_spec, "type_name", type_name);
+    Json_add_array_to_object(generic_type_spec, "type_list", type_list);
+    return generic_type_spec;
+}
+
+Json*
+mk_generic_array_type(Json *type_list) {
+    return mk_generic_user_type("ARRAY", type_list);
+}
+
+Json*
+mk_generic_tuple_type(Json *type_list) {
+    return mk_generic_user_type("TUPLE", type_list);
+}
---
+++ src/include/ast.h
@@ -0,0 +1,406 @@
+#include "json.h"
+
+/**
+ * Создает "пустой" узел. Используется для указания отсутствия
+ * значения у какого-либо узла, либо для создания пустого оператора
+ * (например, точка с запятой (;) транслируется в пустой узел)
+ * 
+ * @return Пустой узел
+ */
+Json*
+mk_empty();
+
+/**
+ * Создает узел константы целого числа
+ * 
+ * @param val Значение целого числа
+ * @return Узел с константой целого числа
+ */
+Json*
+mk_int_const(int val);
+
+/**
+ * Создает узел константы символа
+ * 
+ * @param val Числовой код символа (int)
+ * @return Узел с константой символа
+ */
+Json*
+mk_char_const(int val);
+
+/**
+ * Создает узел константы вещественного числа
+ * 
+ * @param val Значение вещественного числа
+ * @return Узел с константой вещественного числа
+ */
+Json*
+mk_real_const(double val);
+
+/**
+ * Создает узел строковой константы
+ * 
+ * @param val Значение строки
+ * @return Узел со строковой константой
+ */
+Json*
+mk_string_const(char *val);
+
+/**
+ * Создает узел константы результата
+ * 
+ * @return Узел с константой результата
+ */
+Json*
+mk_result_const();
+
+/**
+ * Создает узел константы указателя на текущий экземпляр объекта
+ * 
+ * @return Узел с константой указателя на текущий экземпляр объекта
+ */
+Json*
+mk_current_const();
+
+/**
+ * Создает узел булевой константы
+ * 
+ * @param val Значение булевой переменной
+ * @return Узел с булевой константой
+ */
+Json*
+mk_boolean_const(bool val);
+
+/**
+ * Создает узел пустой константы (void)
+ * 
+ * @return Узел пустой константы
+ */
+Json*
+mk_void_const();
+
+/**
+ * Создает узел идентификатора
+ * 
+ * @param ident Значение идентификатора
+ * @return Узел с идентификатором
+ */
+Json*
+mk_ident_lit(char *ident);
+
+/**
+ * Создает узел бинарной операции
+ * 
+ * @param op_name Имя операции
+ * @param left Левый операнд
+ * @param right Правый операнд
+ * @return Узел бинарной операции
+ */
+Json*
+mk_bin_op(char *op_name, Json *left, Json *right);
+
+/**
+ * Создает узел унарной операции
+ * 
+ * @param op_name Имя операции
+ * @param arg Операнд
+ * @return Узел унарной операции
+ */
+Json*
+mk_unary_op(char *op_name, Json *arg);
+
+/**
+ * Создает узел простого вызова с аргументами без указания выладельца метода
+ * 
+ * @param feature_name Имя вызываемой функции
+ * @param args_list Список аргументов
+ * @return Узел простого вызова
+ */
+Json*
+mk_simple_call(char *feature_name, Json *args_list);
+
+/**
+ * Создает пустой список. Применяется для указания аргументов функции,
+ * последовательности стейтментов, последователи альтернативных ветвей ветвления
+ * и так далее
+ * 
+ * @return Пустой список
+ */
+Json*
+mk_list();
+
+/**
+ * Добавляет элемент в список.
+ * 
+ * @param list Список JSON
+ * @param element Добавляемый элемент
+ * @return Список JSON с добавленным элементом
+ */
+Json*
+add_to_list(Json *list, Json *element);
+
+/**
+ * Создает узел простого вызова без аргументов
+ * 
+ * @param feature_name Имя вызываемой функции
+ * @return Узел простого вызова без аргументов
+ */
+Json*
+mk_simple_call_no_args(char *feature_name);
+
+/**
+ * Создает узел вызова предка с аргументами
+ * 
+ * @param args_list Список аргументов
+ * @return Узел вызова предка с аргументами
+ */
+Json*
+mk_precursor_args_call(Json *args_list);
+
+/**
+ * Создает узел вызова метода родителя с тем же именем без аргументов
+ * 
+ * @return Узел вызова метода родителя с тем же именем без аргументов
+ */
+Json*
+mk_precursor_no_args_call();
+
+/**
+ * Создает узел вызова метода с указанием владельца метода
+ * 
+ * @param owner Владелец. Может быть NULL, тогда создается узел с пустым владельцем
+ * @param feature Метод
+ * @return Узел вызова метода с указанием владельца метода
+ */
+Json*
+mk_feature_with_owner_call(Json *owner, Json *feature);
+
+/**
+ * Создает узел вызова функции с неизвестным владельцем.
+ * 
+ * @param feature Метод
+ * @return Узел вызова функции с неизвестным владельцем
+ */
+Json*
+mk_feature_with_unknown_owner_call(Json *feature);
+
+/**
+ * Создает узел доступа по скобкам
+ * 
+ * @param source Источника
+ * @param index Индекса
+ * @return Узел доступа по скобкам
+ */
+Json*
+mk_bracket_access(Json *source, Json *index);
+
+/**
+ * Создает узел выражения "if"
+ * 
+ * @param cond Условие
+ * @param then_expr Узел "then" выражения
+ * @param alt_exprs Список альтернативных выражений (elseif)
+ * @param else_expr Узел "else" выражения
+ * @return Узел выражения "if"
+ */
+Json*
+mk_if_expr(Json *cond, Json *then_expr, Json *alt_exprs, Json *else_expr);
+
+/**
+ * Добавляет узел выражения "elseif" к списку альтернативных выражений
+ * 
+ * @param alts Список альтернативных выражений
+ * @param cond Узел условия
+ * @param expr Узел выражения
+ * @return Обновленный список альтернативных выражений
+ */
+Json*
+add_elseif_expr(Json *alts, Json *cond, Json *expr);
+
+/**
+ * Создает корневой узел программы.
+ * 
+ * @param program Узел программы - список различных узлов
+ * @return Корневой узел программы
+ */
+Json*
+mk_program(Json *program);
+
+/**
+ * Создает узел оператора "if"
+ * 
+ * @param cond Условие
+ * @param then_stmt_list Список операторов "then"
+ * @param alt_stmts Узел списка альтернативных операторов (elseif)
+ * @param else_stmt_list Список операторов "else"
+ * @return Узел оператора "if"
+ */
+Json*
+mk_if_stmt(Json *cond, Json *then_stmt_list, Json *alt_stmts, Json *else_stmt_list);
+
+/**
+ * Добавляет узел "elseif" к списку альтернативных операторов
+ * 
+ * @param alts Список альтернативных операторов
+ * @param cond Условие
+ * @param stmt_list Список операторов
+ * @return Обновленный список альтернативных операторов
+ */
+Json*
+add_elseif_stmt(Json *alts, Json *cond, Json *stmt_list);
+
+/**
+ * Создает узел оператора присваивания
+ * 
+ * @param left Левый операнд
+ * @param right Правый операнд
+ * @return Узел оператора присваивания
+ */
+Json*
+mk_assign_stmt(Json *left, Json *right);
+
+/**
+ * Создает узел оператора цикла
+ * 
+ * @param init_stmt_list Список начальных операторов
+ * @param cond Узел условия
+ * @param body_stmt_list Список операторов тела цикла
+ * @return Узел оператора цикла
+ */
+Json*
+mk_loop_stmt(Json *init_stmt_list, Json *cond, Json *body_stmt_list);
+
+/**
+ * Создает узел оператора "inspect"
+ * 
+ * @param expr Выражение для проверки
+ * @param when_clauses Список операторов "when"
+ * @param else_stmt_list Список операторов "else"
+ * @return Узел оператора "inspect"
+ */
+Json*
+mk_inspect_stmt(Json *expr, Json *when_clauses, Json *else_stmt_list);
+
+/**
+ * Добавляет узел "when" к списку операторов
+ * 
+ * @param when_clauses Список операторов "when"
+ * @param choices Список условий
+ * @param body Список операторов
+ * @return Обновленный список операторов "when"
+ */
+Json*
+add_alt_when_clause(Json *when_clauses, Json *choices, Json *body);
+
+/**
+ * Создает узел интервала выбора для оператора inspect
+ * 
+ * @param start Начальное значение
+ * @param end Конечное значение
+ * @return Узел интервала выбора
+ */
+Json*
+mk_choice_interval(Json *start, Json *end);
+
+/**
+ * Создает узел типа
+ * 
+ * @param type_name Имя типа
+ * @return Узел типа
+ */
+Json*
+mk_type(char *type_name);
+
+/**
+ * Создает узел встроенного типа "INTEGER"
+ * 
+ * @return Узел встроенного типа "INTEGER"
+ */
+Json*
+mk_integer_type();
+
+/**
+ * Создает узел встроенного типа "REAL".
+ * 
+ * @return Узел встроенного типа "REAL"
+ */
+Json*
+mk_real_type();
+
+/**
+ * Создает узел встроенного типа "STRING".
+ * 
+ * @return Узел встроенного типа "STRING"
+ */
+Json*
+mk_string_type();
+
+/**
+ * Создает узел встроенного типа "CHARACTER".
+ * 
+ * @return Узел встроенного типа "CHARACTER"
+ */
+Json*
+mk_character_type();
+
+/**
+ * Создает узел встроенного типа "BOOLEAN"
+ * 
+ * @return Узел встроенного типа "BOOLEAN"
+ */
+Json*
+mk_boolean_type();
+
+/**
+ * Создает узел типа "LIKE"
+ * 
+ * @param like_what Узел, от которого наследуется тип
+ * @return Узел типа "LIKE"
+ */
+Json*
+mk_like_type(Json *like_what);
+
+/**
+ * Создает узел типа "LIKE Current"
+ * 
+ * @return Узел типа "LIKE Current"
+ */
+Json*
+mk_like_current_type();
+
+/**
+ * Создает узел типа "LIKE other_field"
+ * 
+ * @param field_name Имя поля
+ * @return Узел типа "LIKE other_field"
+ */
+Json*
+mk_like_other_field_type(char *field_name);
+
+/**
+ * Создает узел пользовательского обобщенного типа
+ * 
+ * @param type_name Имя типа
+ * @param type_list Список типов
+ * @return Узел обобщенного типа
+ */
+Json*
+mk_generic_user_type(char *type_name, Json *type_list);
+
+/**
+ * Создает узел обобщенного типа "ARRAY".
+ * 
+ * @param type_list Список типов
+ * @return Узел типа "ARRAY"
+ */
+Json*
+mk_generic_array_type(Json *type_list);
+
+/**
+ * Создает узел обобщенного типа "TUPLE".
+ * 
+ * @param type_list Список типов
+ * @return Узел типа "TUPLE"
+ */
+Json*
+mk_generic_tuple_type(Json *type_list);
