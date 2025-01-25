4b300dbd70141a77ca0bb0b8c9edb3657981e939
samedit66 <samedit66@yandex.ru>
2025-01-25 03:31:39 +0300

    chore

--- src/backend/semantic/base.py
+++ src/backend/semantic/base.py
@@ -1,12 +1,4 @@
-from typing import Iterable, Callable
-
-
 class SemanticError(Exception):
     """Корневой класс для всех ошибок,
     случившихся во время семантического анализа
     """
-
-
-def count[A, B](it: Iterable[A], value: B, key: Callable[[A], B] | None = None):
-    mapped = it if key is None else [key(e) for e in it]
-    return mapped.count(value)
